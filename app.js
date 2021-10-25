13cd0f977938a74f878c5f56bb6fa065276f96bb
yuvischwartz <yuval.orderdesk@gmail.com>
2021-10-25 11:24:26 -0400

    app.use('/api/v1', apiRouter); //use api route

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/v1', apiRouter); //use api route 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
