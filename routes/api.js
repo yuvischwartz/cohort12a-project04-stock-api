3c8a35d493776d54b91fa42b3ec8c5a7da48934c
yuvischwartz <yuval.orderdesk@gmail.com>
2021-11-03 22:14:27 -0400

    /search/:symbol

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const { Portfolio, Wallet } = require('../lib/models');
+const yahooStockPrices = require('yahoo-stock-prices');
+
 /* GET home page. */
 //Placeholder route
 router.get('/', function(req, res, next) {
@@ -11,8 +13,12 @@ router.get('/', function(req, res, next) {
 //1. GET / api / v1 / protfolio - Fetch all the rows in the Protfolio table
 //2. POST /api/v1/protfolio - Create an item in the protfolio
 //3.DETETE /api/v1/portfolio - Delete a portfolio item (selling the item)
-//4. GET /api/v1/wallet - this will fetch the current wallet value - 
+
+//4. GET /api/v1/wallet - this will fetch the current wallet value -
 //5. PUT /api/v1/wallet - this will update the value of the money in your wallet  - 
+
+//6. SEARCH 
+// GET /api/v1/search/:symbol //example: http:/localhost:3000/api/v1/search/AAPL
  
 //1
 router.get('/portfolio', async function (req, res, next) {
@@ -21,6 +27,20 @@ router.get('/portfolio', async function (req, res, next) {
     res.json(items);
 });
 
+//6 search endpoint
+router.get('/search/:symbol', async function (req, res, next) {
+    //req.params is goind to have the value of the symbol
+    console.log(req.params)
+    //let items = await Portfolio.findAll({});
+    //res.json({});
+    try {
+        const data = await yahooStockPrices.getCurrentData(req.params.symbol);
+        res.json({ success: true, data: data });
+    } catch (err) {
+        console.log(err)
+        res.json({ success: false, data: {} });
+    }
+});
 
 //2
 
